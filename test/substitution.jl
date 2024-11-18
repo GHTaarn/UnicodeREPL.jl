3d91734ee3bfdf2ddd628864163d522b020d3a6a
GHTaarn <62629455+GHTaarn@users.noreply.github.com>
2024-11-18 06:20:03 +0100

    Fixed incorrect emoji substitution

--- test/substitution.jl
+++ test/substitution.jl
@@ -4,4 +4,5 @@ using UnicodeREPL
 @testset "substitution" begin
     @test UnicodeREPL.substitution("\\u(2560)\\u(2563)ello World!") == "╠╣ello World!"
     @test UnicodeREPL.substitution("\\u(bed) \\u(cab) \\u(feed) \\u(dad) \\u(BEEF)") == "௭ ಫ ﻭ ත 뻯"
+    @test UnicodeREPL.substitution("\\u(1F607) \\u(3b1)\\u(20E8)") == "😇 α⃨"
 end
